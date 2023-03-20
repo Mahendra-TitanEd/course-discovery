b4ce769fb6af35a98e728fd4400de8ae468620d6
Mahendra <MahendraC@TitanEd.com>
2023-03-20 09:32:28 +0000

    Updated Program partner

--- course_discovery/apps/api/serializers.py
+++ course_discovery/apps/api/serializers.py
@@ -1794,7 +1794,7 @@ class ProgramSerializer(MinimalProgramSerializer):
     staff = MinimalPersonSerializer(many=True)
     instructor_ordering = MinimalPersonSerializer(many=True)
     applicable_seat_types = serializers.SerializerMethodField()
-    partner = serializers.SerializerMethodField()
+    program_partner = serializers.SerializerMethodField()
     topics = serializers.SerializerMethodField()
     level = serializers.SerializerMethodField()
     languages = serializers.SerializerMethodField()
@@ -1857,7 +1857,7 @@ class ProgramSerializer(MinimalProgramSerializer):
             return obj.language.code
         return None
 
-    def get_partner(self, obj):
+    def get_program_partner(self, obj):
         if obj.partner:
             return obj.partner.name
         return None
@@ -1872,7 +1872,7 @@ class ProgramSerializer(MinimalProgramSerializer):
             'staff', 'credit_redemption_overview', 'applicable_seat_types', 'instructor_ordering',
             'enrollment_count', 'topics', 'credit_value', 'level', 'start_date', 'end_date', 'enrollment_end',
             'assignment_due', 'certificte_overview', 'overview_2', 'ebooks_overview', 'placement_overview',
-            'is_new', 'price', 'price_text', 'product_id', 'partner',
+            'is_new', 'price', 'price_text', 'product_id', 'program_partner',
         )
 
 
